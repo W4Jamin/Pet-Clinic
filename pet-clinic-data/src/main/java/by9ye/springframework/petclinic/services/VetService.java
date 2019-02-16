b51a9ea2cc568b05cfa33264a5c5ba917eb8eba1
W4Jamin <yinkop@hotmail.com>
2019-02-16 12:51:34 -0500

    Implemented interface for services. Close #5.

---
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package by9ye.springframework.petclinic.services;
+
+import by9ye.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
