1d963780913db691fde677f233bc9b6a5a2ca0e8
W4Jamin <yinkop@hotmail.com>
2019-02-16 18:10:25 -0500

    Refactoring to common curd interface. Close #11

--- pet-clinic-data/src/main/java/by9ye/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package by9ye.springframework.petclinic.services;
 
 import by9ye.springframework.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
