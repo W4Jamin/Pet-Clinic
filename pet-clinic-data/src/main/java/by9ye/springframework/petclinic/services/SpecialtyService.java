158dc49bc2ae720af9f19ad07f1cc0fbc020b8b9
W4Jamin <yinkop@hotmail.com>
2019-02-21 22:37:26 -0500

    Add specialty service map impl. Close #31

---
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package by9ye.springframework.petclinic.services;
+
+import by9ye.springframework.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package by9ye.springframework.petclinic.services.map;
+
+import by9ye.springframework.petclinic.model.Specialty;
+import by9ye.springframework.petclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
