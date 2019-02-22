98e286a8e3a62619464145fff80920176a1d8798
W4Jamin <yinkop@hotmail.com>
2019-02-21 22:12:31 -0500

    Enhanced owner and pet info. Close #33

--- pet-clinic-web/src/main/java/by9ye/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/by9ye/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package by9ye.springframework.petclinic.bootstrap;
 
 import by9ye.springframework.petclinic.model.Owner;
+import by9ye.springframework.petclinic.model.Pet;
 import by9ye.springframework.petclinic.model.PetType;
 import by9ye.springframework.petclinic.model.Vet;
 import by9ye.springframework.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import by9ye.springframework.petclinic.services.PetTypeService;
 import by9ye.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Bin");
         owner1.setLastName("Yin");
+        owner1.setAddress("474 Seymour Rd APT3");
+        owner1.setCity("Cville");
+        owner1.setTelephone("4342829873");
+
+        Pet binsPet = new Pet();
+        binsPet.setPetType(savedDogPetType);
+        binsPet.setOwner(owner1);
+        binsPet.setBirthDate(LocalDate.now());
+        binsPet.setName("baoyuan");
+        owner1.getPets().add(binsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Jiamin");
         owner2.setLastName("Cheng");
+        owner2.setAddress("474 Seymour Rd APT3");
+        owner2.setCity("Cville");
+        owner2.setTelephone("4342829873");
+
+        Pet jiaminsPet = new Pet();
+        jiaminsPet.setName("yuanbao");
+        jiaminsPet.setPetType(savedCatPetType);
+        jiaminsPet.setBirthDate(LocalDate.now());
+        jiaminsPet.setOwner(owner2);
+        owner2.getPets().add(jiaminsPet);
 
         ownerService.save(owner2);
 
