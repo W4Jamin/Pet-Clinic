3ae85905b011601bd25c12936367d4dcb17cad66
W4Jamin <yinkop@hotmail.com>
2019-02-22 20:35:13 -0500

    Annotated base entity. Close #35

--- pet-clinic-data/src/main/java/by9ye/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package by9ye.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass  // base class in JPA
+//标注为@MappedSuperclass的类将不是一个完整的实体类，
+// 他将不会映射到数据库表，但是他的属性都将映射到其子类的数据库字段中。
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
