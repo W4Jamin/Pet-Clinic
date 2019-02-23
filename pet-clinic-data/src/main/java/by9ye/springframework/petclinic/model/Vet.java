5c6d8a5e7175c94d6571582f0f0c8c02d6222f76
W4Jamin <yinkop@hotmail.com>
2019-02-22 20:51:44 -0500

    Updated vet with JPA entity mappings. Close #37

--- pet-clinic-data/src/main/java/by9ye/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/by9ye/springframework/petclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package by9ye.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER) // load everything even if no queries on this
+    @JoinTable(name = "vet_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialties_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
